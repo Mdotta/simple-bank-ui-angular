d4feead5178010730387577f9418e2aaafd2a7d2
Mdotta <md.correa@outlook.com>
2025-12-25 23:16:20 -0300

    criação de form de transação

--- src/app/components/form-nova-transacao/form-nova-transacao.component.ts
+++ src/app/components/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
 export class FormNovaTransacaoComponent {
-
+  valorTransacao: number = 0.0;
+  tipoTransacao: string = "";
+  aoSubmeter(){
+    console.log("Transação submetida no valor de: ", this.valorTransacao);
+  }
 }
